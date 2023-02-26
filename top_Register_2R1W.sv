4a25d55c71bfb7793b1e41669fb98366f90590fb
SharmaPrateek18 <126266854+SharmaPrateek18@users.noreply.github.com>
2023-02-27 01:43:31 +0530

    Update top_Register_2R1W.sv

--- top_Register_2R1W.sv
+++ top_Register_2R1W.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 10:50:43 PM
-// Design Name: 
-// Module Name: top_Register_2R1W
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module top_Register_2R1W( Data_IN, RW, Q1, R1, Q2, R2, 
